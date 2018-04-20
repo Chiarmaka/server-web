d3e37107e3f0de842b2060c35bd0e6f2dd313d8d
Chiamaka <sandra.ikeh@students.plymouth.ac.uk>
2018-04-20 21:32:58 +0100

    update

--- authenticationIntro-master/app.js
+++ authenticationIntro-master/app.js
@@ -1,19 +1,14 @@
 var express = require('express');
 var app = new express();
+var server = require('http').Server(app);
+var io =  require('socket.io')(server);
+var path = require('path');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var router = express.Router();
 var logger = require('morgan');
-var http = require("http");
-
-  app = module.exports.app = express();
-var server = http.createServer(app);
-//server.listen(port, ipAddress);
-var io = require("socket.io").listen(server)
-//var socket = io.listen(server);
-//var io = require("socket.io").listen(server);
 
 //connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/testForAuth');
@@ -51,7 +46,11 @@ app.use('/', routes);
 require('./routes/routes')(router);
 app.use('/api', router);
 
+app.get('/stream', function(req, res){
+	res.sendFile(path.join(__dirname+'/templateLogReg/stream.html'));
+	//res.send('hola');
 
+});
 
 
 // catch 404 and forward to error handler
@@ -71,9 +70,9 @@ app.use(function (err, req, res, next) {
 var Log = require('log'),
 log = new Log('debug')
 
-var port = process.env.PORT || 5555;
+var port = process.env.PORT || 3000;
 
-
+/*
 io.on('connection',function(socket){
  
   socket.on('stream',function(image){
@@ -82,6 +81,28 @@ io.on('connection',function(socket){
       socket.broadcast.emit('stream',image);
 
   });
+}); */
+
+var usr = [];
+io.on('connection', function(socket){
+	
+	socket.emit('myId', usr.length);
+	usr.push(usr.length);
+	
+	io.emit('createUsuarios', usr);
+	
+
+	socket.on('part', function(data){
+		socket.emit('part', data);
+	});
+
+	socket.on('updateImage', function(data){
+		socket.broadcast.emit('updateImage',data);
+	});
+
+	socket.on('disconnect', function () {
+    	
+  	});
 });
 // listen on port 3000
 server.listen(port, function(){
